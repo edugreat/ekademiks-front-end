3f7046cde394606db7a92121f81e0afbaef7854f
edugreat <anyanwuchinedu687@yahoo.com>
2024-05-31 20:24:45 +0100

    Added the file Assessment component that displays academic assessment subject.
    Added the pipe 'SortPipe' which sorts string inputs in ascending alphabetical order.

---
+++ front-end/src/app/sort.pipe.ts
@@ -0,0 +1,29 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: string[] | null):string[] {
+
+    if(value){
+
+      return value.sort((a:string, b:string) => {
+
+        if(a < b){
+          
+          return -1;
+        }else if(a > b){
+
+         
+          return 1;
+        } return 0;
+
+      })
+    }
+
+    return [];
+  }
+
+}
